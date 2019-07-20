fcb73f4e7e8f8f07913ac99e2fec1cae0bb9312f
Lacedelli <lacedelli@outlook.com>
2019-07-20 12:03:54 -0500

    Add finishing touches to the project, mode switcher, clear button, style modifications, true default for resizing grid, even if user cancels out of prompt

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,9 @@
 // Etch-a-sketch code
+// Get DOM elements 
 const container = document.querySelector(".grid-container");
 const clientWidth = document.documentElement.clientWidth;
 const clientHeight = document.documentElement.clientHeight;
-const button = document.getElementsByTagName("button")[0];
+const colorButton = document.getElementsByTagName("button")[0];
 // Define height of container, otherwise the grid doesn't work
 // DOM defines width of container, don't worry about that
 container.style.height = (clientHeight - 55) + "px";
@@ -17,10 +18,14 @@ function createListeners() {
 			
 	squares.forEach((square) => {
 		square.addEventListener('mouseover', () => {
-			// If it's the first time the user hovers onto the square
-			// create an hsl value for it
-			square.style.backgroundColor = `rgb(${(Math.random() * 255) + 1}, 
-					${(Math.random() * 255) + 1},${(Math.random() * 255) + 1})`;
+			// Check for the state of the button and decide how to decorate the div
+			if(colorButton.dataset.color === "rainbow") {
+				square.style.backgroundColor = `rgb(${(Math.random() * 255) + 1}, 
+					${(Math.random() * 255) + 1},${(Math.random() * 255) + 1})`
+			} else {
+				square.style.backgroundColor = "rgb(0,0,0)";
+			}
+			
 				
 		});
 	});
@@ -30,7 +35,9 @@ function createGrid(gridSize) {
 	//Get percentages of the grid for width and height
 	let rectHeightPercent = (1/gridSize) * 100;
 	let rectWidthPercent = (1/gridSize) * 100;
-				
+	
+	// Now I realize loops like this are unnecesary
+	// Since the DOM positions elements.			
 	for(let i = 0; i < gridSize; i++) {
 		for(let u = 0; u < gridSize; u++) {
 			let div = document.createElement("div");
@@ -45,7 +52,9 @@ function createGrid(gridSize) {
 			
 function resizeGrid() {
 	// Ask for new grid size
-	let newGridSize = Number(prompt("Input the new number of cells for the grid" , "14"));		
+	let newGridSize = Number(prompt("Input the new number of cells for the grid" , "14"));
+	// In case the user cancels out of the prompt
+	if(!newGridSize){newGridSize = 14}
 	// Delete the previous Grid
 	let oldGrid = document.querySelectorAll(".grid-member");
 	oldGrid.forEach((square) => {
@@ -56,6 +65,22 @@ function resizeGrid() {
 	createListeners();
 }
 			
-//TODO add colorChange()
+// Check the status of the button to modify it
+function colorChange() {
+	if (colorButton.dataset.color === "black") {
+		colorButton.dataset.color = "rainbow";
+		colorButton.innerText = "Rainbow mode";
+	} else {
+		colorButton.dataset.color = "black";
+		colorButton.innerText = "Black mode";
+	}
+}
 
 //TODO add resetGrid()
+function resetGrid() {
+	let squares = document.querySelectorAll(".grid-member");
+	
+	squares.forEach((square) => {
+		square.style.backgroundColor = null;
+	});
+}
